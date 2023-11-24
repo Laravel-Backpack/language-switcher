b6bdbf58dbf973ec88dcb7b7f1417ded1ec6b3ec
Pedro Martins <pxpm88@gmail.com>
2023-11-24 16:57:42 +0000

    Update config/language-switcher.php
    Co-authored-by: António Almeida <promatik@gmail.com>

--- config/language-switcher.php
+++ config/language-switcher.php
@@ -7,6 +7,6 @@ return [
 
     // when true, if the page where the picker is displayed use the `backpack route prefix` in the route, 
     // we will also add the route prefix on the language-switcher route.
-    // eg: https://your-application.com/admin/set-locale/{locale} and not https://your-application.com/set-locale/{locale} as default
+    // eg: https://domain.com/admin/set-locale/{locale} instead of https://domain.com/set-locale/{locale}
     'use_backpack_route_prefix_on_admin_routes' => false,
 ];
