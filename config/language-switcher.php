208a6bb2ef54eb00d25b9371ad20b509af4b2ca0
Pedro Martins <pxpm88@gmail.com>
2023-11-24 16:57:35 +0000

    Update config/language-switcher.php
    Co-authored-by: António Almeida <promatik@gmail.com>

--- config/language-switcher.php
+++ config/language-switcher.php
@@ -2,7 +2,7 @@
 
 return [
     // when `false` developer should setup their own route for language switcher
-    // default: Route::any('{backpack_prefix?}/set-locale/{locale}', [\Backpack\LanguageSwitcher\Http\Controllers\LanguageSwitcherController::class, 'setLocale'])->name('language-switcher.locale')
+    // check the default route at /vendor/backpack/language-switcher/routes/language-switcher.php
     'setup_routes' => true,
 
     // when true, if the page where the picker is displayed use the `backpack route prefix` in the route, 
