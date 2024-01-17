8a172e8584057b67545678a414f8aa490ed73e62
Pedro X. <pxpm88@gmail.com>
2024-01-17 13:47:49 +0000

    rewrite comment

--- config/language-switcher.php
+++ config/language-switcher.php
@@ -5,8 +5,7 @@ return [
     // check the default route at /vendor/backpack/language-switcher/routes/language-switcher.php
     'setup_routes' => true,
 
-    // when true, if the page where the picker is displayed use the `backpack route prefix` in the route, 
-    // we will also add the route prefix on the language-switcher route.
+    // when true, we will add the route prefix on the language-switcher route.
     // eg: https://domain.com/admin/set-locale/{locale} instead of https://domain.com/set-locale/{locale}
     'use_backpack_route_prefix' => false,
 ];
